634ecde218ce7312ee42946046c7937afe37b42f
OK42OK <krahlolli@gmail.com>
2025-02-06 11:17:58 +0100

    merged feature chat

---
+++ src/ft_transcendence_backend/tournament/logic/tournament_manager.py
@@ -0,0 +1,5 @@
+from game.models.tournament import Tournament, TournamentPlayer, TournamentMatch
+from game.models import Game
+
+class TournamentManager:
+    # ... existing TournamentManager code ... 
