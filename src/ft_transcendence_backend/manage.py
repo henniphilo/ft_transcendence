34ba03c0e54fbee6cdc87e0d1d13d367bbd1a957
Laurent Brusa <wrmultitudes@gmail.com>
2025-02-07 10:17:31 +0100

    debugging some open telemetry

--- src/ft_transcendence_backend/manage.py
+++ src/ft_transcendence_backend/manage.py
@@ -7,6 +7,11 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+    # Import OpenTelemetry configuration if ENABLE_OTEL is set
+    if os.getenv("ENABLE_OTEL"):
+        from .opentelemetry_config import *
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
