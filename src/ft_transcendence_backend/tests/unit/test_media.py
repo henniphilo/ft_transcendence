d2b29ff22298425e7fc4174c0c99e8ea50ef2cfc
Laurent Brusa <wrmultitudes@gmail.com>
2025-03-10 18:10:46 +0100

    try test test

---
+++ src/ft_transcendence_backend/tests/unit/test_media.py
@@ -0,0 +1,12 @@
+# backend/tests/unit/test_media.py
+import pytest
+from ft_transcendence_backend import media 
+
+def test_some_function():
+    result = 3  
+    assert result == 3  
+
+def test_another_function():
+    # result = media.another_function("hello")
+    result = "HELLO"
+    assert "HELLO" in result
