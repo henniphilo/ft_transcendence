a7ea230b2a2ebb3cfe4d6142b43056a4032f54e2
Laurent Brusa <wrmultitudes@gmail.com>
2025-03-10 18:07:05 +0100

    add test

---
+++ src/ft_transcendence_backend/tests/unit/test_utils.py
@@ -0,0 +1,12 @@
+# backend/tests/unit/test_utils.py
+import pytest
+from backend import utils  # Replace with your actual module
+
+def test_some_function():
+    result = 3  # Replace with your function and test data
+    assert result == 3  # Replace with your expected result
+
+def test_another_function():
+    # result = utils.another_function("hello")
+    result = "HELLO"
+    assert "HELLO" in result
