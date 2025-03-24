a067af1b5a7d7fb9fbee7f73236e60e778e6126d
Omio Horo <ohoro@c3a6c6.42berlin.de>
2025-03-24 11:23:34 +0100

    changed email settings to henni's brevo

--- src/ft_transcendence_backend/backend/settings.py
+++ src/ft_transcendence_backend/backend/settings.py
@@ -246,9 +246,9 @@ SIMPLE_JWT = {
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp-relay.brevo.com")
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "849697001@smtp-brevo.com")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "kOmqRp1jT6KrPbWY")
-EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "8881eb001@smtp-brevo.com")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "Z8rOCaLt4b1EwTph")
+EMAIL_USE_TLS = False
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
 
 # logging configuration
